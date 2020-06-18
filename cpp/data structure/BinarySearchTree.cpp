3beb6c3614202d5b859ec411afde4edc7643e850
nishant <nishantshekharsoni@gmail.com>
2020-06-19 01:26:12 +0530

    Binary Search Tree added with insert and search operation

---
+++ cpp/data structure/BinarySearchTree.cpp
@@ -0,0 +1,121 @@
+/*  code :Binary search tree with class
+    Author: Nishant Shekhar Soni
+    Date : 18th june 2020 23:24 pm
+    learning source: youtube.com/mycodeschool
+*/
+#include <iostream>
+using namespace std;
+
+class BST
+{
+private:
+    //Node
+    struct Node
+    {
+        int data;
+        Node *left;
+        Node *right;
+    };
+    //root node
+    Node *rootptr = NULL;
+
+
+    //get root
+    Node *getRoot()
+    {
+        return rootptr;
+    }
+    //set root
+    void setRoot(Node *temp)
+    {
+        rootptr = temp;
+    }
+
+    //creating a node dinamcally
+    Node *creatNode(int val)
+    {
+        Node *temp = new Node();
+
+        temp->data = val;
+        temp->left = NULL;
+        temp->right = NULL;
+
+        return temp;
+    }
+
+    //inserting a node in tree
+    Node *insertNode(int val, Node *root)
+    {
+        // first case: if tree is empity
+        //base case
+        if (root == NULL)
+        {
+            root = creatNode(val);
+        }
+        else if (val <= root->data)
+        {
+            //recursive call
+            root->left = insertNode(val, root->left);
+        }
+        else
+        {
+            //recursive call
+            root->right = insertNode(val, root->right);
+        }
+        return root;
+    }
+
+    //private function for search
+    bool search(int val, Node *root)
+    {
+        if (root == NULL)
+        {
+            return false; //base case
+        }
+
+        if (val == root->data)
+        {
+            return true;
+        }
+
+        else if (val <= root->data)
+        {
+            return search(val, root->left);
+        }
+        else
+        {
+            return search(val, root->right);
+        }
+    }
+public:
+
+    void insert(int val)
+    {
+        setRoot(insertNode(val, getRoot()));
+    }
+    bool find(int val)
+    {
+        return search(val , getRoot());
+    }
+};
+
+int main()
+{
+    BST t;
+    t.insert(15);
+    t.insert(10);
+    t.insert(20);
+    t.insert(25);
+    t.insert(8);
+    t.insert(12);
+
+    cout<<"is 8 is there ::  "<<t.find(15)<<endl;
+    cout<<"is 10 is there ::  "<<t.find(10)<<endl;
+    cout<<"is 12 is there ::  "<<t.find(12)<<endl;
+
+    cout<<"is 4 is there ::  "<<t.find(4)<<endl;
+    cout<<"is 644 is there ::  "<<t.find(644)<<endl;
+    cout<<"is 836 is there ::  "<<t.find(836)<<endl;
+
+    return 0;
+}
